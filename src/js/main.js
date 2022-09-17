a354b6e0cea12543f52906f1afe3d64bf2d95226
Slawek01~ <lilcrunker@gmail.com>
2022-09-17 23:07:28 +0200

    added dynamicly change date in footer and opening navigation in mobile wersion

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
-const button = document.querySelector(".button")
+const navMobile =document.querySelector('.nav-mobile')
+const hamburgerBtn = document.querySelector(".hamburger")
+const footerYear = document.querySelector(".footer__year")
+
+
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText=year;
+}
+handleCurrentYear()
+
+const showNav = () => {
+   hamburgerBtn.classList.toggle("is-active")
+   navMobile.classList.toggle('nav-mobile__active')
+}
+
+hamburgerBtn.addEventListener('click', showNav)
