912a2bb56733588b4b3f41c63c654bcec976b24c
Slawek01~ <lilcrunker@gmail.com>
2022-09-21 22:42:16 +0200

    finished site

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 const navMobile =document.querySelector('.nav-mobile')
 const hamburgerBtn = document.querySelector(".hamburger")
 const footerYear = document.querySelector(".footer__year")
+const body = document.querySelector("body")
 
 
 const handleCurrentYear = () => {
@@ -12,8 +13,20 @@ handleCurrentYear()
 const showNav = () => {
    hamburgerBtn.classList.toggle("is-active")
    navMobile.classList.toggle('nav-mobile__active')
+   disableScroll()
+
 }
 
+
+
+const disableScroll = () => {
+    navMobile.classList.toggle("active");
+    if(navMobile.classList.contains('active')){
+        body.style.overflow = "hidden";
+    } else{
+        body.style.overflow = "auto"
+    }
+}
 
 
 
